add0fc4baea63769753dd5beb14b2bee5f4749d1
RiccardoBiondi <riccardo.biondi7@unibo.it>
2025-11-19 16:56:12 +0100

    [ENH] Add custom colors for european projects

--- myplotlib/colors/namedcolors.py
+++ myplotlib/colors/namedcolors.py
@@ -2,4 +2,23 @@
 MYCOLORS = {
     "pale_turquoise" : "#adffd6"
 } 
- 
+
+
+SYNTHIA_COLORS = {
+    "synthia_purple": "#6F1BBA",
+    "synthia_gey"   : "#A5A5A5",
+    "synthia_red"   : "#FF4333",
+    "synthia_violet": "#A130F6",
+    "synthia_cyan"  : "#00CED1",
+    "synthia_green" : "#70AD47",
+    "synthia_blue"  : "#003366"
+}
+
+SYNTHEMA_COLORS = {
+    "synthema_red": "#FE3A81",
+    "synthema_orange": "#FF764B",
+    "synthema_yellow": "#FFC206"}
+
+
+MYCOLORS.update(SYNTHIA_COLORS)
+MYCOLORS.update(SYNTHEMA_COLORS)
